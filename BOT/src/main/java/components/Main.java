c9a62009882bedb63de62d96708b1299e5512a8b
Josh <jnievelstein@yahoo.com>
2023-01-27 14:21:03 +0100

    update: structure

--- BOT/src/main/java/components/Main.java
+++ BOT/src/main/java/components/Main.java
@@ -1,15 +1,12 @@
-package components;
+package ressources;
 
 public class Main {
-    static String csvFile = "BOT/src/main/db/fourHeader.csv";
-
+    static String csvFile = "src/main/db/fourHeader.csv";
     // random generated csv file from  https://www.convertcsv.com/generate-test-data.htm
     public static void main(String[] args) {
         try {
             Bot bot = new Bot("Bot", 1, "Admin", csvFile);
             bot.startBot();
-            //Connector connector = new Connector(multicastPORT);
-            //connector.connect();
         } catch (Exception e) {
             e.printStackTrace();
             main(args);
