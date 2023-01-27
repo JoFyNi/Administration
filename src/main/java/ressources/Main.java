ff2818e0c6b613c695e0afd8c523c2edce6f3af5
Josh <jnievelstein@yahoo.com>
2023-01-27 14:21:03 +0100

    update: structure

--- src/main/java/ressources/Main.java
+++ src/main/java/ressources/Main.java
@@ -3,7 +3,6 @@ package ressources;
 public class Main {
     static String csvFile = "src/main/db/fourHeader.csv";
     // random generated csv file from  https://www.convertcsv.com/generate-test-data.htm
-
     public static void main(String[] args) {
         try {
             Bot bot = new Bot("Bot", 1, "Admin", csvFile);
