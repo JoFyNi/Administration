1880f6ab6cdc0585df1df4aba70f4f8df57908c6
Josh <jnievelstein@yahoo.com>
2023-02-01 10:10:14 +0100

    update: created the artefact for the project

---
+++ out/artifacts/Administration_jar/adminTool.bat
@@ -0,0 +1,5 @@
+@echo off
+set USERNAME=%USERPROFILE%
+echo Starting the application with user: %USERNAME%
+java -jar "C:\Users\%USERNAME%\Java\Administration\out\artifacts\Administration_jar"
+pause
