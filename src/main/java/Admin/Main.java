6f83f428b0facca4e92e3a8c332402445094ef5d
Josh <jnievelstein@yahoo.com>
2023-02-02 17:20:16 +0100

    new Communication API

--- src/main/java/Admin/Main.java
+++ src/main/java/Admin/Main.java
@@ -9,8 +9,8 @@ public class Main {
         try {
             administrator admin = new administrator("admin", 1, "admin", csvFile);
             admin.startAdmin();
-            AdminConnector adminConnector = new AdminConnector(multicastPORT);
-            adminConnector.connect();
+            //AdminConnector adminConnector = new AdminConnector(multicastPORT);
+            //adminConnector.connect();
         } catch (Exception e) {
             e.printStackTrace();
             main(args);
