fc3da32ee2c34c1fdf42307d46bc69f76fc7f8ab
Josh <jnievelstein@yahoo.com>
2023-05-10 14:19:42 +0200

    Monitoring.java added

--- AdminGUI/src/main/java/Monitoring.java
+++ AdminGUI/src/main/java/Monitoring.java
@@ -7,6 +7,23 @@ import java.io.File;
 
 public class Monitoring {
 
+    /*
+    String values:
+    eq - Equal to value
+    ne - Not Equal to value
+    ew - Ends with value
+    sw - Starts with value
+    ct - Contains value (substring)
+    re - Regular expression value matches
+    operator is one of the following for numeric values:
+    eq - Equal to value
+    ne - Not Equal to value
+    gt - Greater than value
+    ge - Greater than or equal value
+    lt - Less than value
+    le - Less than or equal value
+     */
+
     public void monitorSystem() {
         while (true) {
             try {
@@ -46,6 +63,58 @@ public class Monitoring {
             }
         }
     }
+
+    //public static void MonitorSystem() throws Exception {
+
+    //    Sigar sigar = new Sigar();
+
+    //    // get the system metrics using Sigar
+    //    Mem mem = sigar.getMem();
+    //    CpuPerc cpu = sigar.getCpuPerc();
+    //    FileSystemUsage fileSystemUsage = sigar.getFileSystemUsage("/");
+    //    double loadAverage = sigar.getLoadAverage()[0];
+
+    //    // create a JMXTrans configuration
+    //    List<Query> queries = new ArrayList<Query>();
+    //    List<String> targetServers = new ArrayList<String>();
+
+    //    Query query = new Query();
+    //    query.setObj("java.lang:type=OperatingSystem");
+    //    query.addAttr("SystemLoadAverage");
+
+    //    queries.add(query);
+    //    targetServers.add("localhost:9999");
+
+    //    JmxTransExporter exporter = new JmxTransExporter();
+    //    exporter.setQueries(queries);
+    //    exporter.setTargetServers(targetServers);
+
+    //    List<OutputWriter> outputWriters = new ArrayList<OutputWriter>();
+
+    //    // send the system metrics to console output
+    //    ConsoleWriter consoleWriter = new ConsoleWriter();
+    //    consoleWriter.addTypeName("typeNames");
+    //    consoleWriter.setSettings(new HashMap<String, Object>());
+    //    outputWriters.add(consoleWriter);
+
+    //    // send the system metrics to a Graphite server
+    //    GraphiteWriter graphiteWriter = new GraphiteWriter();
+    //    graphiteWriter.setHost("graphite.example.com");
+    //    graphiteWriter.setPort(2003);
+    //    graphiteWriter.setDebugEnabled(false);
+    //    graphiteWriter.setSettings(new HashMap<String, Object>());
+    //    outputWriters.add(graphiteWriter);
+
+    //    exporter.setOutputWriters(outputWriters);
+    //    exporter.start();
+
+    //    // print the system metrics to console output
+    //    System.out.println("System Load Average: " + loadAverage);
+    //    System.out.println("Free Memory: " + mem.getFree() / (1024 * 1024) + " MB");
+    //    System.out.println("CPU Usage: " + cpu.getCombined() * 100 + " %");
+    //    System.out.println("File System Usage: " + fileSystemUsage.getUsePercent() + " %");
+    //}
+
 
     private void sendMessage(String s) {
         System.out.println(s);
